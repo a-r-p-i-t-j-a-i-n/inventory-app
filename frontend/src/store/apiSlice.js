3b1ea26b68357817337d983ec03a4971c0b3b387
a-r-p-i-t-j-a-i-n <arpitjian@gmail.com>
2026-01-31 17:01:12 +0530

    intrerate with deploy backend url

--- frontend/src/store/apiSlice.js
+++ frontend/src/store/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: import.meta.env.VITE_API_URL,
+        baseUrl: import.meta.env.VITE_API_URL || (import.meta.env.DEV ? 'http://localhost:5000/api' : 'https://inventory-app-8p88.onrender.com/api'),
         prepareHeaders: (headers, { getState }) => {
             const token = getState().auth.userInfo?.token;
             if (token) {
